222f7af8702d7dc475bcbd642022f7e489c516bf
sasha <eatincobalt@mail.ru>
2026-07-11 20:40:13 +0300

    libs, basic structure, PedroDrivetrain

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/PedroDrivetrain.java
@@ -0,0 +1,58 @@
+package org.firstinspires.ftc.teamcode.subsystems;
+
+import com.pedropathing.follower.Follower;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.seattlesolvers.solverslib.command.RunCommand;
+import com.seattlesolvers.solverslib.command.SubsystemBase;
+
+import org.firstinspires.ftc.teamcode.util.BarnRobot;
+import org.firstinspires.ftc.teamcode.util.Constants;
+
+public class PedroDrivetrain extends SubsystemBase {
+    private DcMotor leftFront;
+    private DcMotor rightFront;
+    private DcMotor leftBack;
+    private DcMotor rightBack;
+
+    public Follower follower;
+
+    private double speedModifier = FAST_SPEED;
+
+    private static final double SLOW_SPEED = 0.5;
+    private static final double FAST_SPEED = 1.0;
+
+    public PedroDrivetrain(OpMode opMode) {
+        leftFront = BarnRobot.getInstance().hardware.leftFrontDrivetrain;
+        rightFront = BarnRobot.getInstance().hardware.rightFrontDrivetrain;
+        leftBack = BarnRobot.getInstance().hardware.leftBackDrivetrain;
+        rightBack = BarnRobot.getInstance().hardware.rightBackDrivetrain;
+        initMotor(DcMotorSimple.Direction.REVERSE, leftFront);
+        initMotor(DcMotorSimple.Direction.FORWARD, rightFront);
+        initMotor(DcMotorSimple.Direction.REVERSE, leftBack);
+        initMotor(DcMotorSimple.Direction.FORWARD, rightBack);
+        follower = Constants.createFollower(opMode.hardwareMap);
+    }
+
+    private void initMotor(DcMotorSimple.Direction direction, DcMotor motor) {
+        motor.setDirection(direction);
+        motor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        motor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        motor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+    }
+
+    private void driveFollower() {
+        follower.update();
+        follower.setTeleOpDrive(
+                BarnRobot.getInstance().gamepadEx1.getLeftY() * speedModifier,
+                -BarnRobot.getInstance().gamepadEx1.getLeftX() * speedModifier,
+                -BarnRobot.getInstance().gamepadEx1.getRightX() * speedModifier,
+                false
+        );
+    }
+
+    public RunCommand driveFollowerCommand() {
+        return new RunCommand(this::driveFollower, this);
+    }
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/util/Constants.java
@@ -0,0 +1,64 @@
+package org.firstinspires.ftc.teamcode.util;
+
+import com.pedropathing.control.FilteredPIDFCoefficients;
+import com.pedropathing.control.PIDFCoefficients;
+import com.pedropathing.follower.Follower;
+import com.pedropathing.follower.FollowerConstants;
+import com.pedropathing.ftc.FollowerBuilder;
+import com.pedropathing.ftc.drivetrains.MecanumConstants;
+import com.pedropathing.ftc.localization.constants.PinpointConstants;
+import com.pedropathing.paths.PathConstraints;
+import com.qualcomm.hardware.gobilda.GoBildaPinpointDriver;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
+import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
+
+public class
+Constants {
+
+    public static FollowerConstants followerConstants = new FollowerConstants()
+            .mass(14.07)
+            .forwardZeroPowerAcceleration(-25.76395763804099)
+            .lateralZeroPowerAcceleration(-87.59485280046445)
+            .useSecondaryTranslationalPIDF(true)
+            .useSecondaryHeadingPIDF(true)
+            .useSecondaryDrivePIDF(true)
+            .translationalPIDFCoefficients(new PIDFCoefficients(0.3, 0, 0.02, 0.04))
+            .secondaryTranslationalPIDFCoefficients(new PIDFCoefficients(0.3, 0.01, 0.01, 0.015))
+            .headingPIDFCoefficients(new PIDFCoefficients(0.15, 0, 0.015, 0.5))
+            .secondaryHeadingPIDFCoefficients(new PIDFCoefficients(5, 0, 0.05, 0.01))
+            .drivePIDFCoefficients(new FilteredPIDFCoefficients(0.035, 0, 0.00008, 0.008, 0.6))
+            .secondaryDrivePIDFCoefficients(new FilteredPIDFCoefficients(0.02, 0, 0.000005, 0.01, 0.6));
+    public static PathConstraints pathConstraints = new PathConstraints(0.99, 100, 1, 1);
+
+    public static Follower createFollower(HardwareMap hardwareMap) {
+        return new FollowerBuilder(followerConstants, hardwareMap)
+                .pathConstraints(pathConstraints)
+                .mecanumDrivetrain(driveConstants)
+                .pinpointLocalizer(localizerConstants)
+                .build();
+    }
+
+    public static MecanumConstants driveConstants = new MecanumConstants()
+            .maxPower(1)
+            .rightFrontMotorName(Hardware.RIGHT_FRONT_DRIVETRAIN_CONFIG_NAME)
+            .rightRearMotorName(Hardware.RIGHT_BACK_DRIVETRAIN_CONFIG_NAME)
+            .leftRearMotorName(Hardware.LEFT_BACK_DRIVETRAIN_CONFIG_NAME)
+            .leftFrontMotorName(Hardware.LEFT_FRONT_DRIVETRAIN_CONFIG_NAME)
+            .leftFrontMotorDirection(DcMotorSimple.Direction.REVERSE)
+            .leftRearMotorDirection(DcMotorSimple.Direction.REVERSE)
+            .rightFrontMotorDirection(DcMotorSimple.Direction.FORWARD)
+            .rightRearMotorDirection(DcMotorSimple.Direction.FORWARD)
+            .xVelocity(73.24950450987328)
+            .yVelocity(53.103720837690695);
+
+    public static PinpointConstants localizerConstants = new PinpointConstants()
+            .forwardPodY(-47/25.4)
+            .strafePodX(-70/25.4)
+            .distanceUnit(DistanceUnit.INCH)
+            .hardwareMapName("pinpoint")
+            .encoderResolution(GoBildaPinpointDriver.GoBildaOdometryPods.goBILDA_4_BAR_POD)
+            .forwardEncoderDirection(GoBildaPinpointDriver.EncoderDirection.FORWARD)
+            .strafeEncoderDirection(GoBildaPinpointDriver.EncoderDirection.FORWARD);
+}
