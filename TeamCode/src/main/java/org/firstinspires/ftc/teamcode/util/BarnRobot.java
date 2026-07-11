3cbe1b3885f6bd9a73f54aa11cef4e7ad0ebfaa8
sasha <eatincobalt@mail.ru>
2026-07-11 20:40:13 +0300

    libs, basic structure, PedroDrivetrain

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/util/BarnRobot.java
@@ -0,0 +1,40 @@
+package org.firstinspires.ftc.teamcode.util;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.seattlesolvers.solverslib.gamepad.GamepadEx;
+
+import org.firstinspires.ftc.robotcore.external.Telemetry;
+import org.firstinspires.ftc.teamcode.subsystems.PedroDrivetrain;
+
+public class BarnRobot {
+    private static BarnRobot instance;
+
+    public Telemetry telemetry;
+
+    public GamepadEx gamepadEx1;
+
+    public Hardware hardware;
+
+    public PedroDrivetrain drive;
+
+    public static boolean isRobotInitialized = false;
+
+    public static synchronized BarnRobot getInstance() {
+        if (instance == null) {
+            instance = new BarnRobot();
+            isRobotInitialized = true;
+        }
+        return instance;
+    }
+
+    public void init(OpMode opMode){
+        this.hardware = new Hardware(opMode.hardwareMap);
+        gamepadEx1 = new GamepadEx(opMode.gamepad1);
+        drive = new PedroDrivetrain(opMode);
+    }
+
+    public void periodic() {
+        telemetry.update();
+        //pinpoint.update();
+    }
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/util/Hardware.java
@@ -0,0 +1,43 @@
+package org.firstinspires.ftc.teamcode.util;
+
+import static org.firstinspires.ftc.robotcore.external.BlocksOpModeCompanion.hardwareMap;
+
+import com.qualcomm.hardware.gobilda.GoBildaPinpointDriver;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
+public class Hardware {
+
+    private HardwareMap hwMap = hardwareMap;
+
+    public static final String LEFT_FRONT_DRIVETRAIN_CONFIG_NAME = "leftFrontDrivetrain";
+    public static final String RIGHT_FRONT_DRIVETRAIN_CONFIG_NAME = "rightFrontDrivetrain";
+    public static final String LEFT_BACK_DRIVETRAIN_CONFIG_NAME = "leftBackDrivetrain";
+    public static final String RIGHT_BACK_DRIVETRAIN_CONFIG_NAME = "rightBackDrivetrain";
+
+    private static final String PINPOINT_CONFIG_NAME = "pinpoint";
+
+    public GoBildaPinpointDriver pinpoint;
+
+    public DcMotor leftFrontDrivetrain;
+    public DcMotor rightFrontDrivetrain;
+    public DcMotor leftBackDrivetrain;
+    public DcMotor rightBackDrivetrain;
+
+    public Hardware(HardwareMap hwMap) {
+        this.hwMap = hwMap;
+        initMotors();
+        initSensors();
+    }
+
+    private void initMotors() {
+        leftFrontDrivetrain = hwMap.get(DcMotor.class, LEFT_FRONT_DRIVETRAIN_CONFIG_NAME);
+        rightFrontDrivetrain = hwMap.get(DcMotor.class, RIGHT_FRONT_DRIVETRAIN_CONFIG_NAME);
+        leftBackDrivetrain = hwMap.get(DcMotor.class, LEFT_BACK_DRIVETRAIN_CONFIG_NAME);
+        rightBackDrivetrain = hwMap.get(DcMotor.class, RIGHT_BACK_DRIVETRAIN_CONFIG_NAME);
+    }
+
+    private void initSensors(){
+        pinpoint = hwMap.get(GoBildaPinpointDriver.class, PINPOINT_CONFIG_NAME);
+    }
+}
