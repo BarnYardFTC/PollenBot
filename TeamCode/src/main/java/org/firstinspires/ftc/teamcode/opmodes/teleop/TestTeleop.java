45c37b8f78e1050ae8b6541aa57c4c51f6225756
sasha <eatincobalt@mail.ru>
2026-07-13 16:48:50 +0300

    fix to static opmodedata

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/teleop/TestTeleop.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/teleop/TestTeleop.java
@@ -9,12 +9,11 @@ import org.firstinspires.ftc.teamcode.opmodes.OpmodeData;
 
 @TeleOp(name = "Test Teleop", group = "test")
 public class TestTeleop extends CommandOpMode {
-    private final OpmodeData opmodeData = new OpmodeData(OpmodeData.AllianceColor.RED, OpmodeData.simple2D(72, 72, 0));
     private final BarnRobot robot = BarnRobot.getInstance();
 
     @Override
     public void initialize() {
-        TeleopTemplate template = new TeleopTemplate(this, opmodeData);
+        TeleopTemplate template = new TeleopTemplate(this);
     }
 
     @Override
