5a10389ee5335e0b1548969e3ad9049b8b601ea6
big (as)S <barnyard22993@gmail.com>
2026-08-14 10:20:11 +0300

    it has to be this way

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/OpmodeData.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/OpmodeData.java
@@ -1,19 +1,19 @@
-package org.firstinspires.ftc.teamcode.opmodes;
-
-import com.pedropathing.geometry.Pose;
-
-import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
-import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
-import org.firstinspires.ftc.robotcore.external.navigation.Pose2D;
-
-public class OpmodeData {
-    public enum AllianceColor { RED, BLUE }
-    public static AllianceColor color;
-    public static Pose2D pose2DSetter = new Pose2D(DistanceUnit.INCH, 0, 0, AngleUnit.DEGREES, 0);
-    public static Pose2D initialPose2D =new Pose2D(DistanceUnit.INCH, 0, 0, AngleUnit.RADIANS, 0) ;
-//    public static Pose initialPose = new Pose(pose2DSetter.getX(DistanceUnit.INCH), pose2DSetter.getY(DistanceUnit.INCH), pose2DSetter.getHeading(AngleUnit.RADIANS));
-
-    public static void updatePose(Pose2D pose2D){
-        initialPose2D = pose2D;
-    }
-}
+//package org.firstinspires.ftc.teamcode.opmodes;
+//
+//import com.pedropathing.geometry.Pose;
+//
+//import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+//import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
+//import org.firstinspires.ftc.robotcore.external.navigation.Pose2D;
+//
+//public class OpmodeData {
+//    public enum AllianceColor { RED, BLUE }
+//    public static AllianceColor color;
+//    public static Pose2D pose2DSetter = new Pose2D(DistanceUnit.INCH, 0, 0, AngleUnit.DEGREES, 0);
+//    public static Pose2D initialPose2D =new Pose2D(DistanceUnit.INCH, 0, 0, AngleUnit.RADIANS, 0) ;
+////    public static Pose initialPose = new Pose(pose2DSetter.getX(DistanceUnit.INCH), pose2DSetter.getY(DistanceUnit.INCH), pose2DSetter.getHeading(AngleUnit.RADIANS));
+//
+//    public static void updatePose(Pose2D pose2D){
+//        initialPose2D = pose2D;
+//    }
+//}
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Scoop.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Scoop.java
@@ -3,38 +3,40 @@ package org.firstinspires.ftc.teamcode.subsystems;
 import com.qualcomm.robotcore.hardware.Servo;
 import com.seattlesolvers.solverslib.command.Command;
 import com.seattlesolvers.solverslib.command.InstantCommand;
-import com.seattlesolvers.solverslib.command.ParallelCommandGroup;
 import com.seattlesolvers.solverslib.command.SubsystemBase;
-import com.seattlesolvers.solverslib.command.WaitCommand;
 
 import org.firstinspires.ftc.teamcode.general.BarnRobot;
 
 public class Scoop extends SubsystemBase {
-    private final Servo rightScoopServo;
-    private final Servo leftScoopServo;
+    private final Servo leftServo;
+    private final Servo rightServo;
 
-
-    private final double SCOOP_COLLECT = 0.05;
-    private final double SCOOP_DUMP = 0.6;
+    private final double SCOOP_COLLECT = 0.2;
+    private final double SCOOP_DUMP = 0.4;
 
     public Scoop() {
-        leftScoopServo = BarnRobot.getInstance().hardware.leftScoopSeervo;
-        rightScoopServo = BarnRobot.getInstance().hardware.rightScoopServo;
-        rightScoopServo.setDirection(Servo.Direction.FORWARD);
-        leftScoopServo.setDirection(Servo.Direction.REVERSE);
-        rightScoopServo.setPosition(SCOOP_COLLECT);
-//        leftScoopServo.setPosition(SCOOP_DUMP);
+        leftServo = BarnRobot.getInstance().hardware.leftScoopServo;
+        leftServo.setDirection(Servo.Direction.FORWARD);
+        leftServo.setPosition(SCOOP_COLLECT);
+        rightServo = BarnRobot.getInstance().hardware.rightScoopServo;
+        rightServo.setDirection(Servo.Direction.FORWARD);
+        rightServo.setPosition(SCOOP_COLLECT);
     }
 
-    public Command collectCommand() {
-        return
-        new InstantCommand(() -> rightScoopServo.setPosition(SCOOP_COLLECT));
-//                    new InstantCommand(() ->  leftScoopServo.setPosition(SCOOP_COLLECT)));
+    private void collect() {
+        leftServo.setPosition(SCOOP_COLLECT);
+        rightServo.setPosition(SCOOP_COLLECT);
+    }
 
+    private void dump() {
+        leftServo.setPosition(SCOOP_DUMP);
+        rightServo.setPosition(SCOOP_DUMP);
+    }
+    public Command collectCommand() {
+        return new InstantCommand(this::collect, this);
     }
 
     public Command dumpCommand() {
-        return
-                new InstantCommand(() ->  rightScoopServo.setPosition(SCOOP_DUMP));
+        return new InstantCommand(this::dump, this);
     }
 }
