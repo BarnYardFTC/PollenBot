f5e03e727cf36e2f5d235be35406709e97652029
big (as)S <barnyard22993@gmail.com>
2026-08-20 13:06:08 +0300

    Natan: After that Nir changed all the electronics and Leon added a new Tranfer Motor, I implimented the new motor into the code

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Transfer.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Transfer.java
@@ -4,26 +4,37 @@ import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.seattlesolvers.solverslib.command.Command;
 import com.seattlesolvers.solverslib.command.InstantCommand;
+import com.seattlesolvers.solverslib.command.ParallelCommandGroup;
 import com.seattlesolvers.solverslib.command.SubsystemBase;
 
 import org.firstinspires.ftc.teamcode.general.BarnRobot;
 
 
 public class Transfer extends SubsystemBase {
-    private final DcMotor transferMotor;
+    private final DcMotor transferTopMotor;
+    private final DcMotor transferBottomMotor;
 
     public Transfer() {
-        transferMotor = BarnRobot.getInstance().hardware.transfer;
-        transferMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
-        transferMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.FLOAT);
-        transferMotor.setDirection(DcMotorSimple.Direction.REVERSE);
+        transferTopMotor = BarnRobot.getInstance().hardware.transferTopMotor;
+        transferTopMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        transferTopMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.FLOAT);
+        transferTopMotor.setDirection(DcMotorSimple.Direction.REVERSE);
+
+        transferBottomMotor = BarnRobot.getInstance().hardware.transferBottomMotor;
+        transferBottomMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        transferBottomMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.FLOAT);
+        transferBottomMotor.setDirection(DcMotorSimple.Direction.REVERSE);
     }
 
     public Command enableCommand(){
-        return new InstantCommand(() -> transferMotor.setPower(1), this);
+        return new ParallelCommandGroup(
+                new InstantCommand(() -> transferTopMotor.setPower(1)),
+                new InstantCommand(() -> transferBottomMotor.setPower(1)));
     }
 
     public Command disableCommand(){
-        return new InstantCommand(() -> transferMotor.setPower(0), this);
+        return new ParallelCommandGroup(
+                new InstantCommand(() -> transferTopMotor.setPower(0)),
+                new InstantCommand(() -> transferBottomMotor.setPower(0)));
     }
 }
