e6cd27f73df803dd947d13f870db1e294523fc8c
Maestro <barnyard22993@gmail.com>
2026-08-23 00:28:37 +0300

    i hate my life

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/teleop/TeleopTemplate.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/teleop/TeleopTemplate.java
@@ -2,8 +2,10 @@ package org.firstinspires.ftc.teamcode.opmodes.teleop;
 
 import static com.seattlesolvers.solverslib.gamepad.GamepadExExtKt.toggleWhenActive;
 
+import com.pedropathing.geometry.Pose;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.seattlesolvers.solverslib.command.Command;
+import com.seattlesolvers.solverslib.command.InstantCommand;
 import com.seattlesolvers.solverslib.command.ParallelCommandGroup;
 import com.seattlesolvers.solverslib.command.RunCommand;
 import com.seattlesolvers.solverslib.command.SequentialCommandGroup;
@@ -23,12 +25,12 @@ public class TeleopTemplate {
     public static void apply(OpMode opMode) {
         PhotonCore.enable();
         robot.init(opMode);
-        //TODO: set to dedicated follower drive once ready
-        robot.drive.setDefaultCommand(robot.drive.driveCommand());
+        robot.drive.setDefaultCommand(robot.drive.drivePollenCommand());
 
         // Binds
         toggleBind(GamepadKeys.Button.B, "Change speed", robot.drive.setSlowModeCommand(),  robot.drive.setFastModeCommand());
-        toggleBind(GamepadKeys.Button.Y, "Scoop", robot.scoop.collectCommand(), robot.scoop.dumpCommand());
+        toggleBind(GamepadKeys.Button.Y, "Scoop", robot.scoop.dumpCommand(),  robot.scoop.collectCommand());
+        toggleBind(GamepadKeys.Button.X, "Go to scoring pose", robot.drive.goToCommand(new Pose(72, 12, 90)), robot.drive.goToCommand(new Pose(72, 12, 90)));
         triggerBind(GamepadKeys.Trigger.RIGHT_TRIGGER, "Intake, transfer", new SequentialCommandGroup(robot.intake.enableCommand(), robot.transfer.enableCommand()), new ParallelCommandGroup(robot.intake.disableCommand(), robot.transfer.disableCommand()));
     }
 
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/teleop/TestTeleop.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/teleop/TestTeleop.java
@@ -1,7 +1,10 @@
 package org.firstinspires.ftc.teamcode.opmodes.teleop;
 
+import com.pedropathing.geometry.Pose;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.seattlesolvers.solverslib.command.CommandOpMode;
+
+import org.firstinspires.ftc.teamcode.general.BarnRobot;
 
 import java.util.ArrayList;
 
@@ -10,12 +13,14 @@ public class TestTeleop extends CommandOpMode {
     @Override
     public void initialize() {
         TeleopTemplate.apply(this);
+        BarnRobot.getInstance().drive.follower.setStartingPose(new Pose(72, 72, 90));
     }
 
     @Override
     public void run() {
         super.run();
         TeleopTemplate.periodic();
+        BarnRobot.getInstance().drive.displayPositionTelemetry();
     }
 
     @Override
